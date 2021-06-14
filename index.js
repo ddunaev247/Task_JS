f8961fd39c4d0b730576f01b63fe04cb283def98
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-14 21:28:49 +0300

    refactor code 2

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 
-
-function checkChar(string){
-    let listLeter = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-    return string
-    .split('')
-    .filter(item => !listLeter.includes(item.toLowerCase()))
-    .join('')
-}
-
 function deleteVowels(str){
+    let listLeter = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
     return str
     .split(' ')
-    .map(item => checkChar(item))
+    .map(item => item
+        .split('')
+        .filter(item => !listLeter.includes(item.toLowerCase()))
+        .join(''))
     .join(' ')
 }
 
-console.log(checkChar('Лол'))
 console.log(deleteVowels('Лол поп КОт Денс! Денис афанасий'))
