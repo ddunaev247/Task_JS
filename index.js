10e0490be2c2a85cba8f9ac4508421369885f72a
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-14 19:29:14 +0300

    refactor code

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-// Тролли атакуют ваш раздел комментариев!
-// Распространенный способ справиться с этой ситуацией - убрать все гласные из комментариев троллей, нейтрализуя угрозу.
-// Ваша задача - написать функцию, которая принимает строку и возвращает новую строку со всеми удаленными гласными.
+
 
 function checkChar(string){
-    let str = string.split('')
-    let newString = []
     let listLeter = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-    for (let i of str){
-        if (!listLeter.includes(i)){
-            newString.push(i)
-        }
-    }
-    return newString.join('')
+    return string
+    .split('')
+    .filter(item => !listLeter.includes(item.toLowerCase()))
+    .join('')
 }
 
-function deleteVowels(str) {
-    let listString = str.split(' ')
-    let resulListtSting = []
-    for (let i of listString){
-        resulListtSting.push(checkChar(i))
-    }
-    return resulListtSting.join(' ')
+function deleteVowels(str){
+    return str
+    .split(' ')
+    .map(item => checkChar(item))
+    .join(' ')
 }
 
-console.log(deleteVowels('Лол поп Кот Денс!'))
+console.log(checkChar('Лол'))
+console.log(deleteVowels('Лол поп КОт Денс! Денис афанасий'))
