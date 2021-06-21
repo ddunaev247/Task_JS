68b227bdfb582626e969d9f5d45c9dba9bfd7d28
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-21 22:28:51 +0300

    add check length

--- task18.js
+++ task18.js
@@ -1,4 +1,5 @@
-const palind = str => str === str.split('').reverse().join('')
+//Напишите функцию, которая проверит, является ли строка палиндромом.
 
+const palind = str => str.length>1?str === str.split('').reverse().join(''):'маленькая строка'
 
 console.log(palind('ololo'))
