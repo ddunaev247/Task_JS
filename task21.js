710b5de046f393957708a686ca2359a51bdebd16
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-21 23:41:13 +0300

    add task21

---
+++ task21.js
@@ -0,0 +1,10 @@
+let createDreamTeam = (members) => {
+
+let result = members.filter(item => typeof item === 'string')
+return  result.length > 0 ? result
+    .map(item => item[0]).sort((a,b) => a.localeCompare(b))
+    .join(''):false
+}
+
+console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]))
+console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']))
