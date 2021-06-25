972c2a490f99cd4fb3b340bbb5f6d9f6631c8395
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-25 19:03:11 +0300

    add comment

--- task29.js
+++ task29.js
@@ -1,3 +1,5 @@
+// Напишите функцию unique_letters(str), которая возвращает строку, оставив в ней только уникальные символы, т.е. встречающиеся в строке один раз.
+
 const unique_letters = str => Array.from(new Set(str.split(''))).join('')
 
 console.log(unique_letters('anaconda'))
