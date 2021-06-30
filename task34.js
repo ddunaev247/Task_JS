7ece3c31d20e70bd47468d3f552218f7ef7807cc
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-06-30 15:37:58 +0300

    add task34

---
+++ task34.js
@@ -0,0 +1,17 @@
+//Создайте метод объекта `String endsWith()`, который сравнивает подстроку str1 с окончанием исходной строки str и определяет,
+// заканчивается ли строка символами подстроки.
+
+String.prototype.endsWith = function(end){
+    return this.split(' ').reverse().indexOf(end)===0
+     
+
+
+
+}
+
+let str = "Каждый охотник желает знать";
+let str1 = "скрипт";
+let str2 = "знать";
+
+console.log(str.endsWith(str2))
+console.log(str.endsWith(str1))
