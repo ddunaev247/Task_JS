c06c1bdcb03c94320b669c20cf71abc111a91828
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-07-03 14:08:02 +0300

    add modif23

--- task37.js
+++ task37.js
@@ -11,7 +11,28 @@ console.log(removeDuplicates2(["php", "php", "css", "css", "script", "script", "
 
 // вариант 3
 
-const removeDuplicates3 = arr =>
+const removeDuplicates3 = arr =>{
+
+    let arrLengthElem = [];
+    let uniqueArr = Array.from( new Set(arr))
+
+        for (let i=0;i<uniqueArr.length;i++){
+            pushElem = arr.filter(item=> item===uniqueArr[i])
+            arrLengthElem.push(pushElem.length)
+            }   
+
+    let minElem = 1
+    let resultArr = []
+
+        for (let i = 0;i<arrLengthElem.length;i++){
+            if (minElem>=arrLengthElem[i]){
+                resultArr.push(uniqueArr[i])
+            }
+        }
+    return resultArr
+}
+
+console.log(removeDuplicates3(["php", "php", "css", "css", "script", "script", "html", "html", "java", 'python' ]))
 
 
 
