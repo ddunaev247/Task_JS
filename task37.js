8d6880a451778375b1778f080e67db4bf7c6c52e
ddunaev247 <Daniil.Dunaev@life.com.by>
2021-07-03 13:53:26 +0300

    add modif2

--- task37.js
+++ task37.js
@@ -5,5 +5,13 @@ const removeDuplicates = arr => Array.from(new Set(arr))
 console.log(removeDuplicates(["php", "php", "css", "css", "script", "script", "html", "html", "java" ]))
 
 //вариант 2
+const removeDuplicates2 = arr => arr.filter((item,index,array)=>array.indexOf(item)===array.lastIndexOf(item))
+
+console.log(removeDuplicates2(["php", "php", "css", "css", "script", "script", "html", "html", "java" ]))
+
+// вариант 3
+
+const removeDuplicates3 = arr =>
+
 
 
